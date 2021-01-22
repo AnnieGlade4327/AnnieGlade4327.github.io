db5704b8477d4d2b0779fb11d426a1b638e12311
Annie <58996371+AnnieGlade4327@users.noreply.github.com>
2021-01-21 23:22:55 -0700

    forgot to add

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//when "submit" is pressed, I want the user's data to be logged in the console.
 async function getUsers() 
 {
   let response = await fetch('https://jsonplaceholder.typicode.com/users')
